8bf894b752de11e73e70a756800b976483fa09d3
isse <sanchezclarisse@gmail.com>
2016-08-27 21:40:19 +0800

    made stock detail dynamic

--- app/src/main/java/com/sam_chordas/android/stockhawk/detail_flow/StockDetailFragment.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/detail_flow/StockDetailFragment.java
@@ -1,10 +1,14 @@
 package com.sam_chordas.android.stockhawk.detail_flow;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +16,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.data.QuoteColumns;
+import com.sam_chordas.android.stockhawk.data.QuoteProvider;
 import com.sam_chordas.android.stockhawk.ui.ChartFragment;
 
 import java.util.ArrayList;
@@ -22,14 +28,22 @@ import java.util.ArrayList;
  * in two-pane mode (on tablets) or a {@link StockDetailActivity}
  * on handsets.
  */
-public class StockDetailFragment extends Fragment {
+public class StockDetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
     /**
      * The fragment argument representing the item ID that this fragment
      * represents.
      */
     public static final String ARG_SYMBOL = "symbol";
+    private static final int CURSOR_LOADER_ID = 0;
 
     private String mSymbol = "";
+
+
+    private TextView txt_symbol;
+    private TextView txt_company;
+    private TextView txt_price;
+    private TextView txt_percent;
+    private TextView txt_change_amt;
 
 
     /**
@@ -50,6 +64,7 @@ public class StockDetailFragment extends Fragment {
             mSymbol = getArguments().getString(ARG_SYMBOL);
 
         }
+
     }
 
     @Override
@@ -57,11 +72,26 @@ public class StockDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.stock_detail, container, false);
 
-        ((TextView) rootView.findViewById(R.id.txt_symbol)).setText(mSymbol);
+        txt_symbol = (TextView) rootView.findViewById(R.id.txt_symbol);
+        txt_symbol.setText(mSymbol);
+
+        txt_company = (TextView) rootView.findViewById(R.id.txt_company);
+        txt_price = (TextView) rootView.findViewById(R.id.txt_price);
+        txt_percent = (TextView) rootView.findViewById(R.id.txt_percent);
+        txt_change_amt = (TextView) rootView.findViewById(R.id.txt_change_amt);
 
         setupTabs(rootView);
 
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // initialize loader
+        getActivity().getSupportLoaderManager().initLoader(CURSOR_LOADER_ID, null, this);
+        super.onStart();
+
     }
 
     private void setupTabs(View rootView) {
@@ -85,6 +115,57 @@ public class StockDetailFragment extends Fragment {
         In your case, you want to specify 2, so that when you are on the third page, the first one is not destroyed, and vice-versa.
         * */
         mViewPager.setOffscreenPageLimit(2);
+
+    }
+
+    @Override
+    public Loader onCreateLoader(int id, Bundle args) {
+        // Attach loader to our flavors database query
+        // run when loader is initialized
+
+        CursorLoader loader = null;
+
+        if(id==CURSOR_LOADER_ID){
+            loader =  new CursorLoader(getContext(), QuoteProvider.Quotes.CONTENT_URI,
+                    new String[]{QuoteColumns._ID, QuoteColumns.SYMBOL, QuoteColumns.NAME, QuoteColumns.BIDPRICE,
+                            QuoteColumns.PERCENT_CHANGE, QuoteColumns.CHANGE
+                    },
+                    QuoteColumns.SYMBOL + " = \"" + mSymbol + "\"",
+                    null,
+                    QuoteColumns._ID + " DESC");
+        }
+
+
+        return loader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        // Set the cursor in our CursorAdapter once the Cursor is loaded
+
+        if (loader.getId() == CURSOR_LOADER_ID && cursor != null && cursor.moveToFirst()) {
+
+            int columnCompanyIndex = cursor.getColumnIndex(QuoteColumns.NAME);
+            final String company = cursor.getString(columnCompanyIndex);
+            txt_company.setText(company);
+
+            int columnPriceIndex = cursor.getColumnIndex(QuoteColumns.BIDPRICE);
+            final String price = cursor.getString(columnPriceIndex);
+            txt_price.setText(price);
+
+            int columnPercentIndex = cursor.getColumnIndex(QuoteColumns.PERCENT_CHANGE);
+            final String percent = cursor.getString(columnPercentIndex);
+            txt_percent.setText(percent);
+
+            int columnChangeIndex = cursor.getColumnIndex(QuoteColumns.CHANGE);
+            final String change = cursor.getString(columnChangeIndex);
+            txt_change_amt.setText(change);
+        }
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader loader) {
 
     }
 
